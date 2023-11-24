106580b58e20ce60178eefc47d76e2be2429c737
AZubaidi <abdulrahman.zubaidi@gmail.com>
2023-11-23 22:26:26 -0500

    points seeds

--- migrations/20231123223351_create_destinations_table.js
+++ migrations/20231123223351_create_destinations_table.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
     table.increments('id').primary();
     table.string('name').notNullable();
     table.string('country');
+    table.string('photo').notNullable();
     table.string('category').notNullable();
   }
   
